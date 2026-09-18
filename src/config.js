55bdc1d375dfbd2f63ac6dfedcb6194036f8f69c
OmarAlhusain <omiero004@gmail.com>
2026-09-18 06:21:24 -0400

    Configure backend API URL

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
-export const API_URL = window.location.hostname === "localhost" ? "<add your theia server side url>" : "add your theia server side url";
-console.log(
-    "API_URL :",
-    API_URL
-);
+export const API_URL =
+  window.location.hostname === "localhost"
+    ? "https://omiero004-8181.theadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/"
+    : "https://omiero004-8181.theadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
+
+console.log("API_URL :", API_URL);
